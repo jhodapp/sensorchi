34cd66b8f2f20ae75aab760f819772867632ebef
Jim Hodapp <james.hodapp@gmail.com>
2021-01-03 12:04:03 -0600

    No longer any need for the BLE logger and some other cleanup.

--- src/sensorchi.ino
+++ src/sensorchi.ino
@@ -6,7 +6,6 @@
  */
 
 #include "air_purity_sensor.h"
-#include "ble_logger.h"
 #include "dust_sensor.h"
 #include "humidity_sensor.h"
 #include "temperature_sensor.h"
@@ -26,11 +25,6 @@ DustSensor dust_sensor;
 HumiditySensor humidity_sensor;
 TemperatureSensor temp_sensor;
 
-// This sets up the BLE log handler. The <4096> template parameter sets the size of the buffer to hold log data
-// The other parameters are like SerialLogHandler. You can set the log level (optional) to things like
-// LOG_LEVEL_ALL, LOG_LEVEL_TRACE, LOG_LEVEL_DEBUG, LOG_LEVEL_INFO, etc.. You can also pass a log filter here.
-//BleLogging<4096> bleLogHandler(LOG_LEVEL_INFO);
-
 SerialLogHandler serialLogHandler(LOG_LEVEL_INFO);
 
 TCPClient client;
@@ -45,7 +39,6 @@ static void DisplayOledSplash();
 static void DisplayOledReadings();
 static void LogReadings();
 static void ReadSensors();
-static void CreateAndSendEventPayload(float temp, float humidity, int pressure, float dust_concentration, const String &air_purity);
 static void CreateAndSendSensorsLocal();
 
 // setup() runs once, when the device is first turned on.
@@ -130,23 +123,6 @@ static void ReadSensors()
     dust_concentration = dust_sensor.read();
 }
 
-static void CreateAndSendEventPayload(float temp, float humidity, int pressure, float dust_concentration, const String &air_purity)
-{
-    JsonWriterStatic<256> jw;
-
-    {
-        JsonWriterAutoObject obj(&jw);
-
-        jw.insertKeyValue("temp", temp);
-        jw.insertKeyValue("humidity", humidity);
-        jw.insertKeyValue("pressure", pressure);
-        jw.insertKeyValue("dust-concentration", dust_concentration);
-        jw.insertKeyValue("air-purity", air_purity);
-    }
-
-    Particle.publish("sensor-readings", jw.getBuffer(), PRIVATE);
-}
-
 static void CreateAndSendSensorsLocal()
 {
     const byte server[] = { 10, 0, 1, 13 };
@@ -163,7 +139,7 @@ static void CreateAndSendSensorsLocal()
         Log.info("TCPClient connected");
         Log.info("ip address: %s", WiFi.localIP().toString().c_str());
         Log.info("gateway: %s", WiFi.gatewayIP().toString().c_str());
-#ifdef USE_JSON_PAYLOAD
+
         Log.info("Sending POST request:");
         Log.info("POST /sensors HTTP/1.1");
         Log.info("User-Agent: argon/0.0.1");
@@ -172,7 +148,6 @@ static void CreateAndSendSensorsLocal()
         Log.info("Content-Length: %d", 39);
         //Log.info("Content-Length: %d", jw.getBufferLen());
         Log.info(jw.getBuffer());
-
 
         client.print("POST /api/sensors HTTP/1.1\r\n");
         client.print("Host: 10.0.1.13:4000\r\n");
@@ -186,27 +161,6 @@ static void CreateAndSendSensorsLocal()
         client.print("\r\n");
         client.print(jw.getBuffer());
         client.print("\r\n");
-#else
-        Log.info("Sending GET request:");
-        //Log.info("GET /hello/Argon HTTP/1.1");
-        Log.info("GET /sensors?temp=%f;humidity=%f HTTP/1.1", temp, humidity);
-        Log.info("Host: 10.0.1.13:4000");
-        Log.info("User-Agent: argon");
-        Log.info("Accept: */*");
-        Log.info("Content-Length: 0");
-
-        //client.print("GET /hello/argon HTTP/1.1\r\n");
-        client.print("GET /sensors?temp=");
-        client.print(temp, 10);
-        client.print("&humidity=");
-        client.print(humidity, 10);
-        client.print(" HTTP/1.1\r\n");
-        client.print("Host: 10.0.1.13:4000\r\n");
-        client.print("User-Agent: argon/0.0.1\r\n");
-        client.print("Accept: */*\r\n");
-        client.print("Content-Length: 0\r\n");
-        client.print("\r\n");
-#endif
     } else {
         Log.error("TCPClient could not connect to endpoint");
     }
